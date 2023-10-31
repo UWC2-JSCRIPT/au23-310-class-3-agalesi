d2c54122f9d8cbad914e39f611af82bc3f4bfa56
agalesi <81053127+agalesi@users.noreply.github.com>
2023-10-31 20:51:35 +0100

    Update itemizedReceipt.js

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -3,16 +3,23 @@
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
 
+const logReceipt = (...items) => {
 
+  let totalPrice = 0
+  items.forEach((item)=>{totalPrice += item.price
+  console.log(item.descr)
+})
+
+  console.log(totalPrice)
+}
 
 // Check
-logReceipt(
-  { descr: 'Burrito', price: 5.99 },
-  { descr: 'Chips & Salsa', price: 2.99 },
-  { descr: 'Sprite', price: 1.99 }
-);
-// should log something like:
-// Burrito - $5.99
-// Chips & Salsa - $2.99
-// Sprite - $1.99
-// Total - $10.97
+
+
+const object1 = {descr: "Fries", price: 2.99}
+const object2 = {descr: "Burger", price: 5.99}
+const object3 = {descr: "Hot Dog", price: 4.99}
+const object4 = {descr: "Milkshake", price: 3.99}
+
+logReceipt(object1, object2, object3, object4)
+
