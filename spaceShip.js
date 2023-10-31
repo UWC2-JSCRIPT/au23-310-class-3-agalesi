afe433e9484dcc0acef2b2188513c9acb7250587
agalesi <81053127+agalesi@users.noreply.github.com>
2023-10-31 21:01:33 +0100

    Update spaceShip.js

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,26 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
-
+class SpaceShip {
+  constructor(name, topSpeed){
+  this.name = name;
+  this.topSpeed = topSpeed; 
+}
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+
+  accelerate() {
+      const {name, topSpeed}= this 
+      
+      console.log(`${name} moving to ${topSpeed}`) 
+    }
+  }
+
+  const enterprise = new SpaceShip("Enterprise", 1000)
+  const milleniumFalcon = new SpaceShip ("Millenium Falcon", 5000)
+
+  enterprise.accelerate("name", "topSpeed") 
+  milleniumFalcon.accelerate("name", "topSpeed") 
+
