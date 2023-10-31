896d46176ac29928b52d937acc08d3183d0b9754
agalesi <81053127+agalesi@users.noreply.github.com>
2023-10-31 20:55:05 +0100

    Update soccer.js

--- soccer.js
+++ soccer.js
@@ -20,23 +20,44 @@ const getPointsFromResult = function getPointsFromResult(result) {
 // including wins, draws, and losses i.e. 'wwdlw'
 // Returns total number of points won
 
+function getTotalPoints(result) {
+  let arrayResults = []
+  arrayResults = result.split('');
+  console.log(arrayResults);
 
+  let total = 0;
+     
+ //arrayResults.forEach((getPointsFromResult) => {total += getPointsFromResult;})
+  total += arrayResults.forEach((getPointsFromResult)); 
+  return total;
+     
+
+}
 
 // Check getTotalPoints
 console.log(getTotalPoints('wwdl')); // should equal 7
 
-// create orderTeams function that accepts as many team objects as desired, 
+// create orderResults function that accepts as many team objects as desired, 
 // each argument is a team object in the format { name, results }
 // i.e. {name: 'Sounders', results: 'wwlwdd'}
 // Logs each entry to the console as "Team name: points"
 
 
+function orderResults(...args){ 
 
+  let teams = []
+  teams = Array.from(args);
+
+}
+
+function orderTeams(name,results){this.name = name; this.results = results;}
 // Check orderTeams
 orderTeams(
   { name: 'Sounders', results: 'wwdl' },
-  { name: 'Galaxy', results: 'wlld' }
-);
+  { name: 'Galaxy', results: 'wlld' }   
+) 
+
+
 // should log the following to the console:
 // Sounders: 7
 // Galaxy: 4
